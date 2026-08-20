2a063c7f3b23ded606104a88a949f47f64600052
TwoSimsLight <89036663+TwoSimsLight@users.noreply.github.com>
2026-08-20 19:02:30 -0300

    limpando arquivos

--- copy_sneak.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-for i in range(42, 71, 2):
-    src = f'original files/assets/image_{i}.jpg'
-    dst = f'assets/image_{i}.jpg'
-    if os.path.exists(src):
-        shutil.copy(src, dst)
